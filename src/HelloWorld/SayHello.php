c9c26c81c9f718d454043c93b6afe58572b88aa8
sionna <sionnaouattara@gmail.com>
2018-11-28 10:05:06 +0000

    first commit

---
+++ src/HelloWorld/SayHello.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Asus Rog
+ * Date: 11/28/2018
+ * Time: 9:14 AM
+ */
+
+namespace HelloWorld;
+
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
